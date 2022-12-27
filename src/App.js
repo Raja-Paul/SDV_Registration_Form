14099c538eb3746d1ee90338970291c86a31f33b
Raja-Paul <101011683+Raja-Paul@users.noreply.github.com>
2022-12-27 20:10:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import FormInput from "./components/FormInput";
 
-function App() {
+const App = () => {
+  const [values, setValues] = useState({
+    Name: "",
+    Email: "",
+    Roll_Number: "",
+    Department : "",
+    Institute_Email_id: "",
+    Contact_No: "",
+    Payment_Proof :"",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "Name",
+      type: "text",
+      placeholder: "Name",
+      errorMessage:
+        "It is required!",
+      label: "Name",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "Email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "Roll Number",
+      type: "text",
+      placeholder: "Roll Number",
+      label: "Roll Number",
+      errorMessage: "It is required!",
+      required: true,
+    },
+    {
+        id: 4,
+        name: "Department",
+        type: "text",
+        placeholder: "Department",
+        errorMessage:
+          "It is required!",
+        label: "Department",
+        required: true,
+
+    },
+    {
+        id: 5,
+        name: "Institute Email ID",
+        type: "email",
+        placeholder: "Institute Email ID",
+        errorMessage: "It should be a valid email address!",
+        label: "Institute Email ID",
+        required: true,
+    },
+    {
+      id: 6,
+      name: "Contact no.(WhatsApp Number)",
+      type: "tel",
+      placeholder: "Contact no.(WhatsApp Number)",
+      errorMessage: "It is required!",
+      label: "Contact no.(WhatsApp Number)",
+      required: true,
+    },
+    {
+    id: 7,
+    name: "Payment proof",
+    type: "file",
+    placeholder: "Payment proof",
+    errorMessage:
+      "It is required!",
+    label: "Payment proof (UPI ID : 9382432159@paytm )",
+    required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          /> 
+        ))}
+        <button>Submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
